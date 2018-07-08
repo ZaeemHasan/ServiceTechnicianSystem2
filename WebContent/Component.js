d232898ff78d6ca1e827261ba77a76ea42ced158
Mainak Ghosh <mainak.narendrapur@gmail.com>
2018-07-08 05:19:18 +0200

    Compotent.js added

---
+++ WebContent/Component.js
@@ -0,0 +1,12 @@
+jQuery.sap.declare("ZSS18_T2_APPT.Component");
+sap.ui.define(['jquery.sap.global','sap/ui/core/UIComponent'],
+	function(jQuery, UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("ZSS18_T2_APPT.Component", {
+		metadata : {
+			manifest : "json"
+		}
+	});
+	return Component;
+});
